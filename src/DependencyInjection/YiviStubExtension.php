0b5961d8f6923a680d28276d71dc890ec4782ec7
Ivan Yivoff <ivan@yivoff.com>
2022-04-05 17:58:49 +0200

    missing extension

--- src/DependencyInjection/YiviStubExtension.php
+++ src/DependencyInjection/YiviStubExtension.php
@@ -3,12 +3,13 @@
 namespace Yivi\StubBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Yivi\StubBundle\String\Reverser;
 use Yivi\StubBundle\String\ReverserInterface;
 
-class YiviStubExtension
+class YiviStubExtension extends Extension
 {
-
+    
     public function load(array $configs, ContainerBuilder $container)
     {
         $container->register('yivi_stub_bundle.string.reverser')
